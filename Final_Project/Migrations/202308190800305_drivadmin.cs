e5206b4209847849a821dfbc7ef15ecc82c6ad25
Harsimran-singh454 <n01562454@humber.ca>
2023-08-19 04:00:59 -0400

    migraye

---
+++ Final_Project/Migrations/202308190800305_drivadmin.cs
@@ -0,0 +1,18 @@
+﻿namespace Final_Project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class drivadmin : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Drivers", "IsAdmin", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Drivers", "IsAdmin");
+        }
+    }
+}
